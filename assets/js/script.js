ba1eb675d619257b070bf5532d412e5f9d5cd245
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-17 13:01:08 +0200

    feat: bonus done

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,9 @@ const {createApp} = Vue;
 createApp({
     data () {
         return {
-            message: "Hello World"
+            message: "Hello World",
+            image: "./assets/img/img.png",
+            imageAlt: "logo di vue.js"
         }
     }
 }).mount("#app")
